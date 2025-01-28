3d6b61cce028947844e54261ea05fc4d95980a4e
Vicargo <Vicargoapp@gmail.com>
2025-01-28 12:42:59 +0100

    Implement fundme contract without security

--- FundMe.sol
+++ FundMe.sol
@@ -2,7 +2,31 @@
 
 pragma solidity ^0.8.18;
 
+import { PriceConverter } from "./PriceConverter.sol";
+
 contract FundMe {
-    function fund() public {}
-    function withdraw() public {}
+    using PriceConverter for uint256;
+
+    uint256 public minimunUsd = 5e18;
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= minimunUsd, "Didn't send enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        // reset mapping
+        for(uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            addressToAmountFunded[funder] = 0; 
+        }
+        // reset funders array
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Withdraw failed");
+    }
 }
--- PriceConverter.sol
+++ PriceConverter.sol
@@ -2,14 +2,16 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
 
     function getPrice() internal view returns(uint256) {
+        // Address of ETH/USD chainlink smart contract
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         (, int256 price,,,) = priceFeed.latestRoundData();
-
+        // Price of ETH in USD
+        // 3000,00000000
         return uint256(price * 1e10);
     }
 
